925ae5978d33bcf3e580a971c0177ff8d51b6a86
Rishita Mukherjee <51188766+rishita2701@users.noreply.github.com>
2020-08-26 10:42:23 +0530

    create

---
+++ code1.R
@@ -0,0 +1,18 @@
+#To return absolute value of input
+x = -55
+print(abs(x))
+#To return square root of input
+y = 400
+print(sqrt(y))
+#To truncate the values of input
+z=7.5555
+print(trunc(z))
+#To return natural logarithm of input
+a=2.25
+print(log(a))
+#To find the maximum value
+print(max(c(42,45,85)))
+#To find the minimum value
+print(min(c(1,8,3,0,6,9)))
+#To find the sum of the given range
+print(sum(c(2,4,6,8,10,15,13,155)))
